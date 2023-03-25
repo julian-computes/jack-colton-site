3d5e7af5daeccb083dd1da094b77790b28a06191
julian-computes <jufer002.work@gmail.com>
2023-03-25 13:55:51 -0400

    Fix missing keys in prop list

--- src/data/posts.tsx
+++ src/data/posts.tsx
@@ -1,18 +1,29 @@
 import TextPost from '@/components/TextPost';
-import VideoPost from '@/components/VideoPost';
 
-const Posts = [
-  (<TextPost title='Lorem ipsum dolor sit'
-             tag='sound'
-             body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cumque debitis eveniet exercitationem illum ipsa laborum minima repellendus, voluptate! Excepturi illo laudantium libero maxime nihil nobis numquam quaerat voluptate.'/>),
-  (<TextPost title='Lorem ipsum dolor sit'
-             tag='visual'
-             body='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cumque debitis eveniet exercitationem illum ipsa laborum minima repellendus, voluptate! Excepturi illo laudantium libero maxime nihil nobis numquam quaerat voluptate.'/>),
-  // (<VideoPost title='fefefefe' url='https://www.youtube.com/embed/CtkajwhHf_U'
-  //             description='This is a description of the video'/>),
-  (<TextPost title='Lorem ipsum dolor sit'
-             tag='sound'
-             body={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cumque debitis eveniet exercitationem illum ipsa laborum minima repellendus, voluptate! Excepturi illo laudantium libero maxime nihil nobis numquam quaerat voluptate.'}/>),
+type PostData = {
+  title: string,
+  tag: tag,
+  body: string,
+}
+
+const PostItems: PostData[] = [
+  {
+    title: 'Lorem ipsum dolor sit',
+    tag: 'sound',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cumque debitis eveniet exercitationem illum ipsa laborum minima repellendus, voluptate! Excepturi illo laudantium libero maxime nihil nobis numquam quaerat voluptate',
+  },
+  {
+    title: 'Lorem ipsum dolor sit',
+    tag: 'visual',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cumque debitis eveniet exercitationem illum ipsa laborum minima repellendus, voluptate! Excepturi illo laudantium libero maxime nihil nobis numquam quaerat voluptate',
+  },
+  {
+    title: 'Lorem ipsum dolor sit',
+    tag: 'sound',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cumque debitis eveniet exercitationem illum ipsa laborum minima repellendus, voluptate! Excepturi illo laudantium libero maxime nihil nobis numquam quaerat voluptate',
+  },
 ]
+
+const Posts = PostItems.map((post, i) => <TextPost title={post.title} body={post.body} tag={post.tag} key={i}/>)
 
 export default Posts
